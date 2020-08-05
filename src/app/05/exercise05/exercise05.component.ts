70c6753b98c7eef971a8ab3bbee6818a692648a1
Pierre Chavaroche <info@chavaroche.de>
2020-08-05 07:48:33 +0200

    Text updated

--- src/app/05/exercise05/exercise05.component.ts
+++ src/app/05/exercise05/exercise05.component.ts
@@ -25,7 +25,7 @@ export class Exercise05Component implements OnInit, AfterViewInit, OnDestroy {
      * 
      * Hints: 
      *    - Use "@ViewChild" to get the input element
-     *    - Use "debounceTime" when listening to the input event
+     *    - Use "debounceTime" and "distinctUntilChanged" when listening to the input event
      *    - Only request JSONPlaceholder servers for users once. Cache the result in private variable.
      *    - Use a "Subject" to emit new search results
      *    - Implement interface "IUser" if necessary
